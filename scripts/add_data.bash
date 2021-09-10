74fd249a65916a18354aa2afed60e2fa5539c7e8
dominikstipic <dominikstipic@gmail.com>
2021-09-10 12:56:52 +0200

    data 0 report 0

--- scripts/add_data.bash
+++ scripts/add_data.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
+DATA_VERSION=$(git log -1 --pretty=%B | head -1 | cut -d " " -f 2)
+REPORT_VERSION=$(git log -1 --pretty=%B | head -2 | tail -1 | cut -d " " -f 2)
+echo data: $DATA_VERSION, report: $REPORT_VERSION
+DATA_VERSION=$((DATA_VERSION + 1))
+REPORT_VERSION=$((REPORT_VERSION))
+
 dvc add data
-git add data.dvc
 dvc push
+
+git add data.dvc .gitignore 
+git commit -m "data $DATA_VERSION 
+report $REPORT_VERSION"
