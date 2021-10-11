5ef708857bf20c5731f5555002b06d2877ff5acd
dominikstipic <dominikstipic@gmail.com>
2021-10-11 12:35:58 +0200

    chore(devops): renamed devops to scripts

---
+++ scripts/weights_to_device.py
@@ -0,0 +1,19 @@
+import argparse
+from pathlib import Path
+
+import torch
+
+parser = argparse.ArgumentParser(description="Convert cuda weights to cpu weights")
+parser.add_argument("--path", default="repository/trainer/output/weights.pt", help="Path to the model weights")
+parser.add_argument("--device", default="cpu", help="A target device")
+parser.add_argument("--out", default="weights.pt", help="name of the file")
+
+
+args = parser.parse_args()
+path = args.path
+device = args.device
+out = args.out
+
+weights = torch.load(path, map_location=torch.device(device))
+output = Path(path).parent / f"{device}_{out}"
+torch.save(weights, str(output))
